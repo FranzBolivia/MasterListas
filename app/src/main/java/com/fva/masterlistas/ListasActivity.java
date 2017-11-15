6cb5f480fddb2bb096f67b39eb564accf67a9e4c
Franz Villalpando <villalpando.franz@usfx.bo>
2017-11-15 15:21:24 -0400

    Initial commit

---
+++ app/src/main/java/com/fva/masterlistas/ListasActivity.java
@@ -0,0 +1,59 @@
+package com.fva.masterlistas;
+
+import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListasActivity extends AppCompatActivity {
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager lManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listas);
+
+//Al presionar el Floatin Button se captura en el con clik y se abre un SnackBar
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+
+        //Inicializar los elementos
+        List items = new ArrayList();
+        items.add(new Lista(R.drawable.trabajo, "Trabajo", 2));
+        items.add(new Lista(R.drawable.casa, "Personal", 3));
+        // Obtener el Recycler
+        recycler = (RecyclerView) findViewById(R.id.reciclador);
+        recycler.setHasFixedSize(true);
+        // Usar un administrador para LinearLayout
+        lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+        // Crear un nuevo adaptador
+        adapter = new ListaAdapter(items);
+        recycler.setAdapter(adapter);
+
+        recycler.addOnItemTouchListener(new RecyclerItemClickListener(ListasActivity.this, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Toast.makeText(ListasActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        }));
+
+
+    }
+}
