82b11c4e490637062116138855d2079bef233113
Franz Villalpando <villalpando.franz@usfx.bo>
2017-11-16 16:56:31 -0400

    Antes de Navigation Drawer

--- app/src/main/java/com/fva/masterlistas/InicioSesionActivity.java
+++ app/src/main/java/com/fva/masterlistas/InicioSesionActivity.java
@@ -1,21 +1,48 @@
 package com.fva.masterlistas;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class InicioSesionActivity extends AppCompatActivity {
+    private TextView bRegistro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        bRegistro = (TextView) findViewById(R.id.registro);
+
+
+
+        bRegistro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                lanzarRegitro(null);
+
+            }
+        });
+
     }
+
+    private void lanzarRegitro(View view) {
+        EditText temporal1 = (EditText) findViewById(R.id.usuario );
+        String temporal = temporal1.getText().toString();
+        Intent i = new Intent(this, Registro.class);
+        i.putExtra("nombre",temporal);
+        startActivity(i);
+
+    }
+
 
     public void loguearCheckbox(View v) {
         CheckBox recordarme = (CheckBox) findViewById(R.id.recordarme);
--- app/src/main/java/com/fva/masterlistas/Registro.java
+++ app/src/main/java/com/fva/masterlistas/Registro.java
@@ -2,12 +2,44 @@ package com.fva.masterlistas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
+ public EditText bCorreo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        Bundle extras = getIntent().getExtras();
+        String s = extras.getString("nombre");
+
+
+
+
+
+        Toast.makeText(this,  s , Toast.LENGTH_LONG).show();
+
+
+
+        EditText b1Correo = (EditText) findViewById (R.id.correo);
+
+        //b1Correo.setText(bCorreo.getText().toString());
+        b1Correo.setText(s);
+
+
+
+
+    }
+
+    public void loguearCheckbox(View v) {
+        CheckBox recordarme = (CheckBox) findViewById(R.id.avisos);
+        String s = "Enviar Avisos: " + (recordarme.isChecked() ? "Sí" : "No");
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 }
